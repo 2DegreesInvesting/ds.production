1ad9b775a0c9e0de2d442a13579b4b13303de04c
Mauro Lepore <maurolepore@gmail.com>
2023-06-16 22:10:16 +0000

    Test 'clean'

--- tests/testthat/_snaps/capture-outputs.md
+++ tests/testthat/_snaps/capture-outputs.md
@@ -12,3 +12,17 @@
       4 Dale  beach       85 US     
       5 Evan  seaside     31 UK     
 
+# `clean` hasn't changed
+
+    Code
+      e$clean
+    Output
+      # A tibble: 5 x 4
+        name  where     temp english
+        <chr> <chr>    <dbl> <chr>  
+      1 Adam  beach     19.4 US     
+      2 Bess  coast     18.2 US     
+      3 Cora  seashore  28   UK     
+      4 Dale  beach     16.4 US     
+      5 Evan  seaside   31   UK     
+
--- tests/testthat/test-capture-outputs.R
+++ tests/testthat/test-capture-outputs.R
@@ -1,6 +1,13 @@
 test_that("`localized` hasn't changed", {
   path <- system.file("extdata", "mvp.Rmd", package = "production")
   e <- new.env()
-  rmarkdown::render(path, envir = e)
+  rmarkdown::render(path, envir = e, quiet = TRUE)
   expect_snapshot(e$localized)
+})
+
+test_that("`clean` hasn't changed", {
+  path <- system.file("extdata", "mvp.Rmd", package = "production")
+  e <- new.env()
+  rmarkdown::render(path, envir = e, quiet = TRUE)
+  expect_snapshot(e$clean)
 })
