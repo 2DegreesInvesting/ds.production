fcec305a7ffd40da18edee4343adfd298981feeb
Mauro Lepore <maurolepore@gmail.com>
2023-06-16 23:46:11 +0000

    New snapshot for celsify_temp()

--- tests/testthat/test-celsify_temp.R
+++ tests/testthat/test-celsify_temp.R
@@ -12,3 +12,15 @@ test_that("without crucial columns errors gracefully", {
   expect_error(celsify_temp(tibble(bad = 32, english = "UK")), "temp.*not TRUE")
   expect_error(celsify_temp(tibble(temp = 32, bad = "UK")), "english.*not TRUE")
 })
+
+test_that("snapshot", {
+  data <- tibble::tribble(
+    ~name,     ~where, ~temp, ~english,
+    "Adam",    "beach",    95,     "US",
+    "Bess",    "coast",    91,     "US",
+    "Cora", "seashore",    28,     "UK",
+    "Dale",    "beach",    85,     "US",
+    "Evan",  "seaside",    31,     "UK"
+  )
+  expect_snapshot(celsify_temp(data))
+})
