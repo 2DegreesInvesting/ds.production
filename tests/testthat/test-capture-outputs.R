9292470c86744dd920ad89c2ff87f852b0e35b3e
Mauro Lepore <maurolepore@gmail.com>
2023-06-16 23:42:34 +0000

    Remove coarse snapshot tests

--- tests/testthat/test-capture-outputs.R
+++
@@ -1,9 +0,0 @@
-test_that("`localized` hasn't changed", {
-  e <- rmd_environment()
-  expect_snapshot(e$localized)
-})
-
-test_that("`clean` hasn't changed", {
-  e <- rmd_environment()
-  expect_snapshot(e$clean)
-})
